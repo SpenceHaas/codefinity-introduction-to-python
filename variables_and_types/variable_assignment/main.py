00bd353a7b7d58e405e8631ff61f013c6ca11d74
SpenceHaas <>
2026-03-04 00:40:29 +0000

    Variable Assignment

---
+++ variables_and_types/variable_assignment/main.py
@@ -0,0 +1,13 @@
+item_name = "Eggs"
+item_price = 3.99
+item_stock_quantity = 120
+in_stock = True
+
+
+
+
+
+print("Item:", item_name)
+print("Price; $", item_price)
+print("Quantity;", item_stock_quantity)
+print("In stock;", in_stock)
